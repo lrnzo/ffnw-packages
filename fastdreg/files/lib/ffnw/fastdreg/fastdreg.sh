e80fe2cc785c8c66440ff5ba9095bc080141cb44
bioxz <bioxz@stderr.cc>
2014-10-04 00:26:36 +0200

    Renamed ffol to ffnw in package fastdreg

--- fastdreg/files/lib/ffnw/fastdreg/fastdreg.sh
+++ fastdreg/files/lib/ffnw/fastdreg/fastdreg.sh
@@ -7,12 +7,12 @@ hostname=$(uci get system.@system[0].hostname)
 
 pubkey=$(/etc/init.d/fastd show_key mesh_vpn)
 
-regdone=$(uci get fastdreg.ffol.regdone)
+regdone=$(uci get fastdreg.ffnw.regdone)
 
 if [ "$regdone" -ne "1" ]; then
 	reg=$(wget -T15 "$keysrv/reg.php?name=$hostname&key=$pubkey" -O -)
 	if [ "$reg" == "regdone" ]; then
-		uci set fastdreg.ffol.regdone=1
+		uci set fastdreg.ffnw.regdone=1
 		uci commit
 	fi
 fi
