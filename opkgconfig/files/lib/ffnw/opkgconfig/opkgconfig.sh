ba8303e0c94993de80c6d7dddc4e0f3021fe880d
Jan-Tarek Butt <buttjantarek@googlemail.com>
2015-05-20 00:45:33 +0200

    add fastdreg config remove function

--- opkgconfig/files/lib/ffnw/opkgconfig/opkgconfig.sh
+++ opkgconfig/files/lib/ffnw/opkgconfig/opkgconfig.sh
@@ -5,6 +5,11 @@ cleanup () {
 	rm -rf /lib/ffnw/opkgconfig
 	}
 
+ls /etc/config/fastdreg > /dev/null
+if [ $? -eq 0 ]; then
+	rm /etc/config/fastdreg
+fi
+
 grep -Fxvf /etc/opkg.conf /lib/ffnw/opkgconfig/opkg.conf > /dev/null
 if [ $? -eq 0 ]; then
 	mv /lib/ffnw/opkgconfig/opkg.conf /etc/opkg.conf
