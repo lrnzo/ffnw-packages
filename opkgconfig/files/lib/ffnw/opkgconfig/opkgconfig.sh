849a3bf004b7df6c63b163f8c3aced55f75c2dae
Jan-Tarek Butt <buttjantarek@googlemail.com>
2015-06-23 12:27:23 +0200

    remove revoving code for fastdreg

--- opkgconfig/files/lib/ffnw/opkgconfig/opkgconfig.sh
+++ opkgconfig/files/lib/ffnw/opkgconfig/opkgconfig.sh
@@ -5,10 +5,6 @@ cleanup () {
 	rm -rf /lib/ffnw/opkgconfig
 	}
 
-if [ -e /etc/config/fastdreg ]; then
-	rm /etc/config/fastdreg
-fi
-
 grep -Fxvf /etc/opkg.conf /lib/ffnw/opkgconfig/opkg.conf > /dev/null
 if [ $? -eq 0 ]; then
 	mv /lib/ffnw/opkgconfig/opkg.conf /etc/opkg.conf
